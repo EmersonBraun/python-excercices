35aff7e0d48e2a120cfc2b5afe000b9c1055679f
EmersonBraun <emersonfbraun@gmail.com>
2019-10-24 16:52:13 -0300

    mixins

---
+++ cod3r/poo_avancado/evolucao_v2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+class Humano:
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-2]
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+
+if __name__ == '__main__':
+    jose = HomoSapiens('Jose')
+    grok = Neanderthal('Grok')
+
+    print(
+        f'Evolução (a partir da classe): {", ".join(HomoSapiens.especies())}')
+    print(f'Evolução (a partir da instancia): {", ".join(jose.especies())}')
+    print(f'Homo Sapiens evoluído? {HomoSapiens.is_evoluido()}')
+    print(f'Neanderthal evoluído? {Neanderthal.is_evoluido()}')
+    print(f'José evoluído? {jose.is_evoluido()}')
+    print(f'Grok evoluído? {grok.is_evoluido()}')
