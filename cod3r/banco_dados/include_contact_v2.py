3254ea808e57e9c43107dea358e06be203220bf6
EmersonBraun <emersonfbraun@gmail.com>
2019-11-04 18:00:11 -0300

    sql with python

--- cod3r/banco_dados/include_contact_v2.py
+++ cod3r/banco_dados/include_contact_v2.py
@@ -2,14 +2,20 @@ from mysql.connector.errors import ProgrammingError
 from db import new_connection
 
 sql = 'INSERT INTO contacts(name, tel) VALUES (%s,  %s)'
-args = ('Lucas', '9876-54321')
+args = (
+    ('Lucas', '9876-54321'),
+    ('Zé', '8876-54321'),
+    ('Maria', '7876-54321'),
+    ('João', '6876-54321'),
+    ('ana', '5876-54321'),
+)
 
 with new_connection() as connection:
     try:
         cursor = connection.cursor()
-        cursor.execute(sql, args)
+        cursor.executemany(sql, args)
         connection.commit()
     except ProgrammingError as e:
         print(f'Error: {e.msg}')
     else:
-        print(f'Insert ID: {cursor.lastrowid}')
+        print(f'Insert {cursor.rowcount} registry')
--- cod3r/banco_dados/select_contacts.py
+++ cod3r/banco_dados/select_contacts.py
@@ -1,15 +1,15 @@
 from mysql.connector.errors import ProgrammingError
 from db import new_connection
 
-sql = 'INSERT INTO contacts(name, tel) VALUES (%s,  %s)'
-args = ('Lucas', '9876-54321')
+sql = 'SELECT * FROM contacts'
 
 with new_connection() as connection:
     try:
         cursor = connection.cursor()
-        cursor.execute(sql, args)
-        connection.commit()
+        cursor.execute(sql)
+        result = cursor.fetchall()
     except ProgrammingError as e:
         print(f'Error: {e.msg}')
     else:
-        print(f'Insert ID: {cursor.lastrowid}')
+        for r in result:
+            print(f'{r[2]:2d} - {r[0]:10s} Tel: {r[1]}')
